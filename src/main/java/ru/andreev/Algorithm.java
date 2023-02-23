4ba58942ba6ec9fab074f24022a9f36c31b59a6c
andreevmo <maxiandreev1994@gmail.com>
2023-02-23 13:31:10 +0400

    feat: add algorithm binary search and test

--- src/main/java/ru/andreev/Algorithm.java
+++ src/main/java/ru/andreev/Algorithm.java
@@ -1,4 +1,26 @@
 package ru.andreev;
 
 public class Algorithm {
+
+    public static Integer binarySearch(Integer[] numbers, Integer elementForSearch) {
+        int low = 0;
+        int high = numbers.length - 1;
+        int index;
+
+        while (low <= high) {
+            index = (low + high) / 2;
+
+            if (numbers[index].equals(elementForSearch)) {
+                return index;
+            } else if (elementForSearch > index) {
+
+                low = index + 1;
+            } else {
+                high = index - 1;
+            }
+
+        }
+
+        return null;
+    }
 }
--- src/test/java/ru/andreev/TestAlgorithms.java
+++ src/test/java/ru/andreev/TestAlgorithms.java
@@ -1,11 +1,17 @@
 package ru.andreev;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class TestAlgorithms {
 
     @Test
-    void testAlgorithm() {
+    void testBinarySearch() {
+        Integer[] numbers = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        Integer result1 = Algorithm.binarySearch(numbers, 11);
+        Assertions.assertNull(result1);
+
+        Integer result2 = Algorithm.binarySearch(numbers, 3);
+        Assertions.assertEquals(2, result2);
     }
 }
