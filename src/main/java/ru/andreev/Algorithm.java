93d8a130c6254ab1db810e4c0fe57d41954c03bc
andreevmo <maxiandreev1994@gmail.com>
2023-02-25 15:55:12 +0400

    feat: add algorithms and test
    Add sum, countElements, findGreatestNum, quickSort in the Algorithm
    Add tests in the TestAlgorithms

--- src/main/java/ru/andreev/Algorithm.java
+++ src/main/java/ru/andreev/Algorithm.java
@@ -57,4 +57,68 @@ public class Algorithm {
         }
         return n * factorial(n - 1);
     }
+
+    public static int sum(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        } else if (arr.length == 1) {
+            return arr[0];
+        } else {
+            int[] arrNew = new int[arr.length - 1];
+            System.arraycopy(arr, 1, arrNew, 0, arrNew.length);
+            return arr[0] + sum(arrNew);
+        }
+    }
+
+    public static int countElements(List<?> list) {
+        if (list.size() == 0) {
+            return 0;
+        } else if (list.size() == 1) {
+            return 1;
+        } else {
+            List<?> li = new ArrayList<>(list);
+            li.remove(0);
+            return 1 + countElements(li);
+        }
+    }
+
+    public static Integer findGreatestNum(List<Integer> numbers) {
+        if (numbers.size() == 0) {
+            return null;
+        } else if (numbers.size() == 1) {
+            return numbers.get(0);
+        } else {
+            List<Integer> list = new ArrayList<>(numbers);
+            if (list.get(0) > list.get(1)) {
+                list.remove(1);
+            } else {
+                list.remove(0);
+            }
+            return findGreatestNum(list);
+        }
+    }
+
+    public static List<Integer> quickSort(List<Integer> list) {
+        if (list.size() < 2) {
+            return list;
+        }
+
+        int index = (int) (Math.random() * list.size());
+        Integer pivot = list.get(index);
+        list.remove(index);
+        List<Integer> less = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+        for (Integer n : list) {
+            if (n > pivot) {
+                greater.add(n);
+            } else {
+                less.add(n);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>(quickSort(less));
+        result.add(pivot);
+        result.addAll(quickSort(greater));
+        return result;
+    }
 }
--- src/test/java/ru/andreev/TestAlgorithms.java
+++ src/test/java/ru/andreev/TestAlgorithms.java
@@ -46,4 +46,81 @@ public class TestAlgorithms {
         Assertions.assertEquals(120, factorial5);
         Assertions.assertEquals(6, factorial3);
     }
+
+    @Test
+    void tetSum() {
+        int[] arr1 = new int[]{4, 9, 1, 12};
+        int sum1 = Algorithm.sum(arr1);
+
+        Assertions.assertEquals(26, sum1);
+
+        int[] arr2 = new int[]{15, 3, 8, 11};
+        int sum2 = Algorithm.sum(arr2);
+
+        Assertions.assertEquals(37, sum2);
+    }
+
+    @Test
+    void testCountElements() {
+        List<Integer> list1 = new ArrayList<>(List.of(1, 5, 6, 3, 7, 3, 5, 2, 6));
+        int count1 = Algorithm.countElements(list1);
+
+        Assertions.assertEquals(9, count1);
+
+        List<Integer> list2 = new ArrayList<>(List.of(1, 5, 6, 3, 7, 2, 6, 8));
+        int count2 = Algorithm.countElements(list2);
+
+        Assertions.assertEquals(8, count2);
+
+        List<Integer> list3 = new ArrayList<>();
+        int count3 = Algorithm.countElements(list3);
+
+        Assertions.assertEquals(0, count3);
+
+        List<Integer> list4 = new ArrayList<>(List.of(1));
+        int count4 = Algorithm.countElements(list4);
+
+        Assertions.assertEquals(1, count4);
+    }
+
+    @Test
+    void testFindGreatestNum() {
+        List<Integer> list1 = new ArrayList<>(List.of(1, 5, 6, 3, 7, 3, 5, 2, 6));
+        Integer count1 = Algorithm.findGreatestNum(list1);
+
+        Assertions.assertEquals(7, count1);
+
+        List<Integer> list2 = new ArrayList<>(List.of(1, 5, 6, 3, 7, 2, 6, 8));
+        Integer count2 = Algorithm.findGreatestNum(list2);
+
+        Assertions.assertEquals(8, count2);
+
+        List<Integer> list3 = new ArrayList<>(List.of(8));
+        Integer count3 = Algorithm.findGreatestNum(list3);
+
+        Assertions.assertEquals(8, count3);
+
+        Integer count4 = Algorithm.findGreatestNum(new ArrayList<>());
+
+        Assertions.assertNull(count4);
+    }
+
+    @Test
+    void testQuickSort() {
+        List<Integer> list1 = new ArrayList<>(List.of(1, 5, 6, 3, 7, 3, 5, 2, 6));
+        List<Integer> listCompare = new ArrayList<>(List.of(1, 2, 3, 3, 5, 5, 6, 6, 7));
+        List<Integer> sortList1 = Algorithm.quickSort(list1);
+
+        Assertions.assertEquals(listCompare, sortList1);
+
+        List<Integer> list2 = new ArrayList<>(List.of(1));
+        List<Integer> sortList2 = Algorithm.quickSort(list2);
+
+        Assertions.assertEquals(list2, sortList2);
+
+        List<Integer> list3 = new ArrayList<>();
+        List<Integer> sortList3 = Algorithm.quickSort(list3);
+
+        Assertions.assertEquals(list3, sortList3);
+    }
 }
